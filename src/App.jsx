484954c388cfc547612205cb6a5209e19ef360cc
Nyunt Sein <nyuntsein.dev@gmail.com>
2025-08-31 15:11:49 -0400

    remove sample function

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,12 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Text display="Hello React" />
-    </div>
+   <>
+   
+   </>
   );
 }
 
-function Text({ display }) {
-  return (
-    <div>
-      <p>{display}</p>
-    </div>
-  );
-}
+
 
 export default App;
