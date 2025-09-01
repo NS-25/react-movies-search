11d455c69d50387500ada5f802131e27e1f0b812
Nyunt Sein <nyuntsein.dev@gmail.com>
2025-09-01 13:29:08 -0400

    create function

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 const MovieCard = ({ movie }) => {
+  const onFavoriteClick = () => {
+    alert("Clicked");
+  };
   return (
     <div className="movie-card">
       <div className="movie-poster">
         <img src={movie.url} alt={movie.title} />
         <div className="movie-overlay">
-          <button className="favorite-btn" onClick={}></button>
+          <button className="favorite-btn" onClick={onFavoriteClick}></button>
         </div>
       </div>
     </div>
