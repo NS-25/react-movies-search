59f2a95b7286d18bc766cfa90ee2153fdce5e553
Nyunt Sein <nyuntsein.dev@gmail.com>
2025-09-01 13:44:53 -0400

    add movie card in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import "./App.css";
+import MovieCard from "./components/MovieCard";
 
 function App() {
   return (
-   <>
-   
-   </>
+    <>
+      <MovieCard movie={{ title: "Nyunt Sein", release_date: "2025" }} />
+    </>
   );
 }
-
-
 
 export default App;
