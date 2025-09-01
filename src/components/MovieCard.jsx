58193aa1095c6233e4734a533aeb65d4731758cf
Nyunt Sein <nyuntsein.dev@gmail.com>
2025-09-01 13:26:25 -0400

    add css for button

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,6 +5,9 @@ const MovieCard = ({ movie }) => {
     <div className="movie-card">
       <div className="movie-poster">
         <img src={movie.url} alt={movie.title} />
+        <div className="movie-overlay">
+          <button className="favorite-btn" onClick={}></button>
+        </div>
       </div>
     </div>
   );
