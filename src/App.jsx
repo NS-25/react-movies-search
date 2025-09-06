9d8bc136adfca16d4e87a5452639929e83081c40
Nyunt Sein <nyuntsein.dev@gmail.com>
2025-09-05 23:48:55 -0400

    basic routes setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
 
 function App() {
   return (
     <main className="main-content">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </main>
   );
